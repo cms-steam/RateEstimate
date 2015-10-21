784730ec2e3e57a89b7ed95896c38380ea0acd0f
Georgia <georgia.karapostoli@gmail.com>
2015-10-21 18:51:51 +0200

    data specific input

---
+++ datasetCrossSections/datasetLumiSectionsData.py
@@ -0,0 +1,26 @@
+datasetList = [
+    'HLTPhysics1',
+    'HLTPhysics2',
+    'HLTPhysics3',
+    'HLTPhysics4'
+]
+
+datasetNegWeightList=[
+]
+datasetAntiMuList= [
+]
+
+datasetAntiEMList= [
+]
+
+datasetEMEnrichedList = [
+]
+
+datasetMuEnrichedList = [
+]
+xsectionDatasets ={
+    'HLTPhysics1' : 1.,
+    'HLTPhysics2' : 1.,
+    'HLTPhysics3' : 1.,
+    'HLTPhysics4' : 1.
+}
