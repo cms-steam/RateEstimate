9f65759607927c251b63d254d511bbe2801f2747
David Vannerom <david.vannerom@cern.ch>
2016-02-22 16:46:57 +0100

    Updated batch submission script without the eos mounting.

--- RatesBatchScript.sh
+++ RatesBatchScript.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
-MYDIR=/afs/cern.ch/user/v/vannerom/work/test/RateEstimate/
+MYDIR=/afs/cern.ch/user/v/vannerom/work/cms-steam/RateEstimate/
+CMSSWDir=/afs/cern.ch/user/v/vannerom/work/CMSSW_7_6_3_patch2/src/
+#FILES=root://eoscms//eos/cms/store/group/dpg_trigger/comm_trigger/TriggerStudiesGroup/STEAM/Spring15/Hui_HLTRates_2e33_25ns_V4p4_V1/*
+FILES=/afs/cern.ch/user/v/vannerom/eos/cms/store/group/dpg_trigger/comm_trigger/TriggerStudiesGroup/STEAM/Spring15/Hui_HLTRates_2e33_25ns_V4p4_V1/*
 
-for i in `seq 0 88`
+for f in $FILES
 do
-#if [ "$i" -eq "17" ] || [ "$i" -eq "27" ] || [ "$i" -eq "33" ] || [ "$i" -eq "42" ] || [ "$i" -eq "44" ] || [ "$i" -eq "47" ] || [ "$i" -eq "50" ] || [ "$i" -eq "53" ] || 
-#if [ "$i" -eq "30" ] || [ "$i" -eq "72" ] || [ "$i" -eq "83" ] || [ "$i" -eq "84" ]
-#then 
-       echo ${i}
-#       str2=$(printf $i)
+fbase=$( basename "$f" )
+#fbase="HLTPhysics1"
+echo $fbase
+#  echo "Processing $fbase file..."
+for i in `seq 1 350`
+do
+  echo ${i}
 
        (                                                                                                                                       
-       cat <<EOF                                                                                                                                     
-curr_dir=`pwd`                                                                                                                                                                                                       
-cd ${MYDIR}                                                                                                                                     
-source /afs/cern.ch/project/eos/installation/cms/etc/setup.sh 
-eosmount /afs/cern.ch/user/v/vannerom/eos                                                                                                                                  
-source env.sh                                                                                                                                   
-python RateEstimateBatch.py -n $i
+      cat <<EOF                                                                                                                                     
+curr_dir=`pwd`                                                                                                                                                                                                                                
+cd ${MYDIR}
+source env.sh
+python RateEstimate.py -n $i -d $fbase
 EOF
-) > submit_${i}.job
+) > submit_${fbase}_${i}.job
 
 
-chmod +x submit_${i}.job 
+chmod +x submit_${fbase}_${i}.job 
 #myfile=submit_$i.job  
-bsub -q 8nh submit_${i}.job #myfile
+#bsub -q 8nh submit_${fbase}_${i}.job #myfile
+bsub -q 8nh -eo err.dat -oo out.dat submit_${fbase}_${i}.job
 #bsub -q 8nh -eo err.dat -oo out.dat -J "myjob"$str2 "python RateEstimate.py -n $i"
-#fi
 done
+done
+
+#cd ${CMSSWDir} 
+#eval `scramv1 runtime -sh`
+#cd -
+#source /cvmfs/cms.cern.ch/slc6_amd64_gcc491/lcg/root/6.02.10/bin/thisroot.sh
